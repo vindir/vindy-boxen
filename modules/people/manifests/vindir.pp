1a11fccf32cd6c2076724551b53d1d8205c6575b
Jamey Owens <jamey.owens@vonage.com>
2015-06-24 12:08:39 -0400

    Add dropbox/coreutils/md5sum

--- modules/people/manifests/vindir.pp
+++ modules/people/manifests/vindir.pp
@@ -1,8 +1,15 @@
 class people::vindir {
   include vonbiz
   include caffeine
+  include dropbox
 
   package { 'watch':
+    ensure => present,
+  }
+  package { 'coreutils':
+    ensure => present,
+  }
+  package { 'md5sha1sum':
     ensure => present,
   }
 }
