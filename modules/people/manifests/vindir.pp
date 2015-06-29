9f9d3057bc23f9fb2ebf9e12a38ede9978d1db44
Jamey Owens <jamey.owens@vonage.com>
2015-06-29 12:47:13 -0400

    Apply sublime text repos

--- modules/people/manifests/vindir.pp
+++ modules/people/manifests/vindir.pp
@@ -2,6 +2,7 @@ class people::vindir {
   include vonbiz
   include caffeine
   include dropbox
+  include sublime_text
 
   package { 'irssi':
     ensure => present,
@@ -14,5 +15,10 @@ class people::vindir {
   }
   package { 'md5sha1sum':
     ensure => present,
+  }
+
+  # For the latest build of v3
+  sublime_text::package { 'Emmet':
+    source => 'sergeche/emmet-sublime'
   }
 }
